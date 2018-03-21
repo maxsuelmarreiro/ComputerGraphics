f25bf99d8ae027048da93c52a82952acec15a07e
Maxsuel Marreiro <maxsuelmarreiro@gmail.com>
2018-03-20 23:24:17 -0300

    Update pdiAlgorithm.h

--- pdiAlgorithm.h
+++ pdiAlgorithm.h
@@ -1,6 +1,8 @@
 #ifndef PDIALGORITHM_H_
 #define PDIALGORITHM_H_
 
+Image * img_View(Image * origImg);
+
 Image * img_ConvertGray(Image * origImg);
 Image * img_Blur(Image * origImg, int sizeMask);
 Image * img_BlurMediana(Image * origImg, int sizeMask);
@@ -9,6 +11,13 @@ Image * img_Negativo(Image * origImg);
 
 Image * img_SaltAndPepper(Image * origImg, int qtd);
 Image * img_Gaussiano(Image * origImg, int qtd);
+
+Image * img_Limiarizacao(Image * origImg, int T);
+Image * img_Binarizacao(Image * origImg);
+
+Image * img_PassaAltasRoberts(Image * origImg);
+Image * img_PassaAltasSobel(Image * origImg);
+Image * img_PassaAltasPrewitt(Image * origImg);
 
 
 unsigned int * img_GetHistogram (Image * img);
